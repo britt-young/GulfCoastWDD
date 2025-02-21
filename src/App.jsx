68a2aa6805bf356a59e678e6d4fca28c11ed98cc
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-21 11:28:39 -0500

    initial install of vite/react template and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
