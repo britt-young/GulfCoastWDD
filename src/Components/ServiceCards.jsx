79e212b71b91faaf21299dece31df63a8deac3c4
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-24 13:48:00 -0500

    service card text

--- src/Components/ServiceCards.jsx
+++ src/Components/ServiceCards.jsx
@@ -5,110 +5,123 @@ import mobile from "../assets/icons/mobile.png";
 
 const ServiceCards = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-5 bg-blue-400">
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
-            <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
-            </Link>
-        </div>
+    <div className="bg-blue-400">
+      <div className="flex flex-col items-center justify-center py-5">
+        <h4 className="">what we offer</h4>
+        <h2 className="text-center text-3xl font-bold">Our Services</h2>
+        <p className="py-10">shgvksuhdgv</p>
       </div>
 
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
+      {/* Feature Card Grid */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-5 ">
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
             <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
+              <FaArrowRight />
             </Link>
+          </div>
+        </div>
+
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
+            <Link to="/services" className="flex items-center justify-end">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
+            <Link to="/services" className="flex items-center justify-end">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
+            <Link to="/services" className="flex items-center justify-end">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
+            <Link to="/services" className="flex items-center justify-end">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+
+        {/* Feature Card */}
+        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <img
+            src={mobile}
+            alt="serviceicon"
+            className="w-16 h-16 mb-5 shadow-xl shadow-blue-300"
+          />
+          <div>
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>kiesurthiseutbhkeiu</p>
+          </div>
+          <div>
+            <Link to="/services" className="flex items-center justify-end">
+              <FaArrowRight />
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
-            <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
-            </Link>
-        </div>
-      </div>
-
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
-            <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
-            </Link>
-        </div>
-      </div>
-
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
-            <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
-            </Link>
-        </div>
-      </div>
-
-      <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
-        <img
-          src={mobile}
-          alt="serviceicon"
-          className="w-16 h-16 mb-5"
-        />
-        <div>
-          <h4 className="text-center">Mobile First Design</h4>
-          <p>kiesurthiseutbhkeiu</p>
-        </div>
-        <div>
-            <Link to="/services" className="flex items-center justify-end">
-                <FaArrowRight />
-            </Link>
-        </div>
-      </div>
-
-
     </div>
   );
 };
