5ac40e49fc78255f01e360fbb454ea8622a3f06d
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-26 08:52:03 -0500

    card styling

--- src/Components/ServiceCards.jsx
+++ src/Components/ServiceCards.jsx
@@ -9,13 +9,13 @@ const ServiceCards = () => {
       <div className="flex flex-col items-center justify-center py-5">
         <h4 className="">what we offer</h4>
         <h2 className="text-center text-3xl font-bold">Our Services</h2>
-        <p className="py-10">shgvksuhdgv</p>
+        <p className="mt-5">shgvksuhdgv</p>
       </div>
 
       {/* Feature Card Grid */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-5 ">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 py-5 lg:mx-80 mx-20">
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
@@ -33,7 +33,7 @@ const ServiceCards = () => {
         </div>
 
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
@@ -51,7 +51,7 @@ const ServiceCards = () => {
         </div>
 
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
@@ -69,7 +69,7 @@ const ServiceCards = () => {
         </div>
 
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
@@ -87,7 +87,7 @@ const ServiceCards = () => {
         </div>
 
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
@@ -105,7 +105,7 @@ const ServiceCards = () => {
         </div>
 
         {/* Feature Card */}
-        <div className="bg-white mx-5 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="bg-white mx-2 lg:mt-0 mt-2 p-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
           <img
             src={mobile}
             alt="serviceicon"
