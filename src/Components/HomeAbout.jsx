f4cec93c758aa37a8e8e51a7b8a132b2eecf1576
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-26 16:49:30 -0500

    btn styling change for accessibilties purposes

--- src/Components/HomeAbout.jsx
+++ src/Components/HomeAbout.jsx
@@ -13,7 +13,7 @@ const HomeAbout = () => {
       </div>
       <div className="flex flex-col items-start lg:ml-8 m-5 lg:w-180">
         <h4 className="">Gulf Coast Guarentee</h4>
-        <h2 className="text-3xl font-bold">Websites starting at $0 down and $145 per month</h2>
+        <h2 className="font-bold">Websites starting at $0 down and $145 per month</h2>
         <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
         <div className="w-full"><GuarenteeGrid/></div>
         <button className="mt-10">About Us</button>
