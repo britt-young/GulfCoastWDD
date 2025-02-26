80d99d85896f542104777642679f96da4324ecb3
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-26 16:36:22 -0500

    guarentee grid styling adjustments

--- src/Components/GuarenteeGrid.jsx
+++ src/Components/GuarenteeGrid.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from "react";
 import { GoCheckCircleFill } from "react-icons/go";
 
 const GuarenteeGrid = () => {
   return (
-    <div>
-        <div className="w-40">
-        <GoCheckCircleFill />
-            <h5 className="text-start">Our Guarentee</h5>
-            <p className="text-start">Lorem ipsum dolor sit amet</p>
-        </div>
-
+    <div className="grid grid-cols-2 gap-10 mt-10">
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      {/* Snippet */}
+      <div className="w-fit ">
+        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GuarenteeGrid
+export default GuarenteeGrid;
