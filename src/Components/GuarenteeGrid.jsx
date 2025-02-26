d00c9776255906650dfdcc7c60e00564366d5bcf
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-26 10:24:46 -0500

    react icon add

--- src/Components/GuarenteeGrid.jsx
+++ src/Components/GuarenteeGrid.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { GoCheckCircleFill } from "react-icons/go";
 
 const GuarenteeGrid = () => {
   return (
-    <div>GuarenteeGrid</div>
+    <div>
+        <div className="w-40">
+        <GoCheckCircleFill />
+            <h5 className="text-start">Our Guarentee</h5>
+            <p className="text-start">Lorem ipsum dolor sit amet</p>
+        </div>
+
+    </div>
   )
 }
 
--- src/Components/HomeAbout.jsx
+++ src/Components/HomeAbout.jsx
@@ -8,10 +8,10 @@ const HomeAbout = () => {
         <img
           src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="about"
-          className="w-full h-200 object-cover pb-10 mr-8"
+          className="w-full h-200 object-cover pb-10 px-5 lg:px-0 mr-8"
         />
       </div>
-      <div className="flex flex-col lg:items-start lg:ml-8 w-180">
+      <div className="flex flex-col lg:items-start lg:ml-8 m-5 lg:w-180">
         <h4 className="">Gulf Coast Guarentee</h4>
         <h2 className="text-3xl font-bold">Websites starting at $0 down and $145 per month</h2>
         <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
