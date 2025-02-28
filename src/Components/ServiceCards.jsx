c6ba643473599d588b6d2aebaf4516b17d72b566
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-02-28 12:16:21 -0500

    service card grid styling and icon add

--- src/Components/ServiceCards.jsx
+++ src/Components/ServiceCards.jsx
@@ -6,7 +6,7 @@ import { MdPhonelinkSetup } from "react-icons/md";
 const ServiceCards = () => {
   return (
     <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="flex flex-col items-center justify-center py-5">
+      <div className="mx-auto max-w-7xl flex flex-col items-center justify-center py-5">
         <h4 className="">what we offer</h4>
         <h2 className="text-center text-3xl font-bold">Our Services</h2>
         <p className="mt-5 lg:w-200 mx-5">
@@ -15,19 +15,18 @@ const ServiceCards = () => {
           minim veniam, quis nostrud exercitation ullamco laboris
         </p>
       </div>
-
       {/* Feature Card Grid */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 py-20 lg:w-300 lg:h-150 overflow-hidden mx-auto">
-        {/* Feature Card (add negative margin on icon to offset?*/}
-        <div className="bg-blue-50 w-fit m-2 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
-          <img
-            src={<MdPhonelinkSetup />}
-            alt="serviceicon"
-            className="rounded-lg bg-blue-900/60 w-16 h-16 mb-4 absolute left-1/2 transform -translate-x-11/12 -top-4"
-          />
-          <div>
+      <div className="mx-auto max-w-7xl grid grid-cols-1 lg:grid-cols-3 lg:gap-8 gap-0 lg:p-20 overflow-hidden">
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
             <h4 className="text-center">Mobile First Design</h4>
-            <p>kiesurthiseutbhkeiu</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
           </div>
           <div>
             <Link to="/services" className="">
@@ -35,8 +34,95 @@ const ServiceCards = () => {
             </Link>
           </div>
         </div>
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
+          </div>
+          <div>
+            <Link to="/services" className="">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
+          </div>
+          <div>
+            <Link to="/services" className="">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
+          </div>
+          <div>
+            <Link to="/services" className="">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
+          </div>
+          <div>
+            <Link to="/services" className="">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        {/* Feature Card */}
+        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col items-center justify-center relative">
+          <MdPhonelinkSetup className="rounded-lg bg-blue-900/60 p-4 w-20 h-20 mb-4 absolute left-1/2 transform -translate-x-1/2 -top-11" />
+          <div className="m-5">
+            <h4 className="text-center">Mobile First Design</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+              odio commodi voluptatibus possimus provident! Quidem magnam, velit
+              provident cupiditate, quasi earum temporibus numquam!
+            </p>
+          </div>
+          <div>
+            <Link to="/services" className="">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        
       </div>
-      <div className="flex flex-col items-center justify-center">
+
+      <div className="flex flex-col items-center justify-center mt-5 lg:mt-0">
         <button className="">get started</button>
       </div>
     </div>
