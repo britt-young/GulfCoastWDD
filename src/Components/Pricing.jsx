f23267d87ad3304ba5c59be2cfba847bc22be27b
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-03 12:52:22 -0500

    bg change

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -58,7 +58,7 @@ const Pricing = () => {
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
-          className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="mx-auto aspect-1155/678 w-full bg-linear-to-tr from-[#e43d3d] to-[#2a95eb] opacity-90"
         />
       </div>
       <div className="mx-auto max-w-4xl text-center">
@@ -77,7 +77,7 @@ const Pricing = () => {
           <div
             key={tier.id}
             className={classNames(
-              tier.featured ? 'relative bg-gray-900 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
+              tier.featured ? 'relative bg-red-800 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
               'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 h-full',
             )}
           >
@@ -96,7 +96,7 @@ const Pricing = () => {
               >
                 {tier.priceMonthly}
               </span>
-              <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base')}>/month</span>
+              <span className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-500', 'text-base')}>/month</span>
             </p>
             <p className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7')}>
               {tier.description}
@@ -123,7 +123,7 @@ const Pricing = () => {
               aria-describedby={tier.id}
               className={classNames(
                 tier.featured
-                  ? 'bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500'
+                  ? 'bg-indigo-500 text-white shadow-xs hover:bg-red-400 focus-visible:outline-indigo-500'
                   : 'text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600',
                 'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
               )}
