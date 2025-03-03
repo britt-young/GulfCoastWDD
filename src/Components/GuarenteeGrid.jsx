e135dca1692d8aebbab161972903e7686a07ecb7
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-03 12:45:27 -0500

    grid styling

--- src/Components/GuarenteeGrid.jsx
+++ src/Components/GuarenteeGrid.jsx
@@ -3,36 +3,66 @@ import { GoCheckCircleFill } from "react-icons/go";
 
 const GuarenteeGrid = () => {
   return (
-    <div className="grid grid-cols-2 gap-10 mt-10">
+    <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 mt-10">
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, mollitia?</p>
       </div>
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, cupiditate?</p>
       </div>
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>{" "}
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, explicabo!</p>
       </div>
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>{" "}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, incidunt.</p>
       </div>
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>{" "}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, natus?</p>
       </div>
       {/* Snippet */}
-      <div className="w-fit ">
-        <h5><span><GoCheckCircleFill className="inline-flex items-center mr-1 mb-1"/></span>Our Guarentee</h5>
-        <p>Lorem ipsum dolor sit amet</p>
+      <div className="w-full ">
+        <h5 className="font-bold">
+          <span>
+            <GoCheckCircleFill className="inline-block me-1 mb-1" />
+          </span>
+          Our Guarentee
+        </h5>{" "}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rerum?</p>
       </div>
     </div>
   );
