6181389c1574ebb4a1871f241c5d8c8893c613d4
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-11 12:06:09 -0400

    contact and business info add

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {PhoneArrowDownLeftIcon, EnvelopeIcon, MapPinIcon} from "@heroicons/react/24/solid";
+
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 bg-teal-600 p-20 lg:px-80 lg:gap-40 gap-20">
+      <div className="flex flex-col lg:items-start justify-between py-5 lg:order-first order-last">
+        <img src="https://via.placeholder.com/150" alt="logo" />
+        <p className="my-5 mr-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris </p>
+          <button>Get Started</button>
+      </div>
+      <div className="flex flex-col lg:items-start justify-between py-5 lg:order-last order-first">
+        <h3 className="mb-5">Contact Us</h3>
+        <p className="my-1"><PhoneArrowDownLeftIcon className="inline-block w-6 me-2" />+123 456 7890</p>
+        <p className="my-1 mr-10"><EnvelopeIcon className="inline-block w-6 me-2" />gulfcoastwdd@protonmail.com</p>
+        <p className="my-1"><MapPinIcon className="inline-block w-6 me-2" />United States</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "../Components/NavBar";
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
 
 const RootLayout = () => {
   return (
-    <div className="bg-blue-900 min-h-screen">{/* THIS IS THE MAIN BG! Use min-h-screen to fill the viewport */}
+    <div className="bg-blue-800 min-h-screen">{/* THIS IS THE MAIN BG! Use min-h-screen to fill the viewport */}
       <Navbar />
-      <main className="">
+      <main>
         <Outlet />
       </main>
-      <div className="bg-black h-[100px] text-white">FOOTER</div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
