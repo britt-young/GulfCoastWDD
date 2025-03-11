51b72f69bd08e050ff38f980f8d0b8290c4158cb
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-11 12:46:17 -0400

    desktop link spacing, mobile dropdown bg and spacing

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,44 +27,35 @@ const NavBar = () => {
   return (
     <div className="py-8 lg:px-40 px-8">
       <nav
-        className={`flex justify-between items-center px-4 transition-all duration-200 ease-in-out ${isScrolled ? "fixed top-0 left-0 w-full bg-white shadow-md z-50 rounded-none m-0 scroll-smooth" : "lg:rounded-3xl rounded-xl bg-white shadow-xl z-50"}`}
+        className={`flex justify-between items-center px-4 transition-all duration-200 ease-in-out ${
+          isScrolled
+            ? "fixed top-0 left-0 w-full bg-white shadow-md z-50 rounded-none m-0 scroll-smooth"
+            : "lg:rounded-3xl rounded-xl bg-white shadow-xl z-50"
+        }`}
       >
         {/* Logo or brand */}
-        <div className="text-xl font-bold">Logo</div>
+        <img src="/logo.png" alt="logo" className="h-12" />
 
         {/* Mobile menu toggle */}
         <button
           className="lg:hidden m-2"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <GiHamburgerMenu />
         </button>
 
         {/* Desktop menu */}
-        <ul className="hidden lg:flex space-x-4 uppercase">
-          <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+        <ul className="hidden lg:flex space-x-10 uppercase">
+          <li className="hover:font-bold hover:text-purple-800">
             <NavLink to="/">Home</NavLink>
           </li>
-          <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+          <li className="hover:font-bold hover:text-purple-800">
             <NavLink to="/About">About</NavLink>
           </li>
-          <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+          <li className="hover:font-bold hover:text-purple-800">
             <NavLink to="/FAQs">FAQs</NavLink>
           </li>
-          <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+          <li className="hover:font-bold hover:text-purple-800">
             <NavLink to="/Contact">Contact</NavLink>
           </li>
         </ul>
@@ -78,36 +70,33 @@ const NavBar = () => {
       </nav>
 
       {/* Mobile menu */}
-      <div className={`lg:hidden ${isMenuOpen ? "block" : "hidden"}`}>
+      <div className={`lg:hidden ${isMenuOpen ? "block" : "hidden"} lg:rounded-3xl rounded-xl bg-white shadow-xl -z-40 -mt-5 py-8`}>
         <ul className="flex flex-col space-y-4 px-4 py-2 uppercase items-center">
-          <li>
-            <NavLink to="/">Page 1</NavLink>
+          <li className="hover:font-bold hover:text-purple-800">
+            <NavLink to="/">Home</NavLink>
           </li>
-          <li>
-            <NavLink to="/About">Page 2</NavLink>
+          <li className="hover:font-bold hover:text-purple-800">
+            <NavLink to="/About">About</NavLink>
           </li>
-          <li>
-            <NavLink to="/FAQs">Page 3</NavLink>
+          <li className="hover:font-bold hover:text-purple-800">
+            <NavLink to="/FAQs">FAQs</NavLink>
           </li>
-          <li>
-            <NavLink to="/Contact">Page 4</NavLink>
-          </li>
-          <li>
-            <NavLink to="/page5">Page 5</NavLink>
+          <li className="hover:font-bold hover:text-purple-800">
+            <NavLink to="/Contact">Contact</NavLink>
           </li>
         </ul>
 
         {/* Sign Up Button (Mobile) */}
-  <div className="flex justify-center mt-5"> 
-    <button
-      className="p-2 w-full max-w-xs cursor-pointer"
-      onClick={() => navigate("/page5")}
-    >
-      Sign Up
-    </button>
+        <div className="flex justify-center mt-5">
+          <button
+            className="p-2 w-full max-w-xs cursor-pointer"
+            onClick={() => navigate("/page5")}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
   );
 };
 
