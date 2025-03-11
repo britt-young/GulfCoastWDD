4bee76cf68fd7e8adec95e2f08ac045a4f5cfa93
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-11 13:24:42 -0400

    home about section refactored to grid

--- src/Components/HomeAbout.jsx
+++ src/Components/HomeAbout.jsx
@@ -3,15 +3,15 @@ import GuarenteeGrid from "./GuarenteeGrid";
 
 const HomeAbout = () => {
   return (
-    <div className="relative isolate bg-red-200 px-6 py-24 sm:py-32 lg:px-8 flex flex-col lg:flex-row items-center justify-center">
-      <div>
+    <div className="relative isolate bg-pink-300 px-6 py-24 sm:py-32 lg:px-8 grid grid-cols-1 lg:grid-cols-2 items-center justify-center">
+      <div className="mx-auto lg:mr-20">
         <img
           src="https://images.pexels.com/photos/20044366/pexels-photo-20044366/free-photo-of-hands-holding-smartphone-over-laptop.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="about"
-          className="w-full h-185 object-cover rounded-2xl"
+          className="w-full lg:h-185 h-140 object-cover rounded-3xl"
         />
       </div>
-      <div className="flex flex-col items-start lg:ml-8 m-5 lg:w-180">
+      <div className="flex flex-col items-start lg:mr-80 mt-10 lg:mt-0">
         <h4 className="">Gulf Coast Guarentee</h4>
         <h2 className="font-bold">Websites starting at $0 down and $145 per month</h2>
         <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
