083f06c24fa1e10f131a099d925489c9d3fd825c
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-11 11:26:34 -0400

    testimonial card styling/margins

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <div className="bg-yellow-300 pt-20">
-      <div className="mx-auto max-w-7xl flex flex-col items-center justify-center py-5">
+    <div className="bg-yellow-300 p-20">
+      <div className="mx-auto max-w-7xl flex flex-col items-center justify-center py-5 mb-10">
         <h4 className="">testimonials</h4>
         <h2 className="text-center text-3xl font-bold">what our clients are saying</h2>
         <p className="mt-5 lg:w-200 mx-5">
@@ -13,9 +13,9 @@ const Testimonials = () => {
         </p>
       </div>
       {/* Testimonial Grid */}
-      <div className="mx-auto  grid grid-cols-1 lg:grid-cols-3 lg:gap-x-2 lg:gap-y-20 gap-0 lg:p-20 pb-4 overflow-hidden">
+      <div className="mx-auto max-w-full grid grid-cols-1 lg:grid-cols-3 p-5 lg:gap-x-5 overflow-hidden">
         {/* Testimonial Card */}
-        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col relative">
+        <div className="bg-blue-50 h-fit m-2 mt-5 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-4xl lg:rounded-2xl flex flex-col relative">
           <div className="my-2 items-start justify-start">
             <h4 className="mt-5">Mobile First Design</h4>
             <p className="mt-2 mb-3">
@@ -26,7 +26,7 @@ const Testimonials = () => {
           </div>
         </div>
         {/* Testimonial Card */}
-        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col relative">
+        <div className="bg-blue-50 h-fit m-2 mt-5 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-4xl lg:rounded-2xl flex flex-col relative">
           <div className="my-2 items-start justify-start">
             <h4 className="mt-5">Mobile First Design</h4>
             <p className="mt-2 mb-3">
@@ -37,7 +37,7 @@ const Testimonials = () => {
           </div>
         </div>
         {/* Testimonial Card */}
-        <div className="bg-blue-50 h-fit m-2 mt-20 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-lg flex flex-col relative">
+        <div className="bg-blue-50 h-fit m-2 mt-5 lg:mt-0 p-8 shadow-lg shadow-gray-500 rounded-4xl lg:rounded-2xl flex flex-col relative">
           <div className="my-2 items-start justify-start">
             <h4 className="mt-5">Mobile First Design</h4>
             <p className="mt-2 mb-3">
