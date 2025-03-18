f6a2af8e7bf78aace70b8dc2cc82f87304289814
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-18 14:36:55 -0400

    padding responsiveness

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeHeader = () => {
   return (
-    <div className="bg-yellow-400 px-6 py-24 sm:py-32 lg:px-80 flex flex-col lg:flex-row items-center justify-center gap-8">
+    <div className="bg-yellow-400 lg:pt-60 pt-40 pb-20 lg:px-80 flex flex-col lg:flex-row items-center justify-center gap-8">
       <div className="h-full items-start justify-center flex flex-col px-5 lg:px-0">
         <h4>unique designs, custom-coded</h4>
         <h2>small business web design</h2>
@@ -14,7 +14,7 @@ const HomeHeader = () => {
           <img
             src="https://plus.unsplash.com/premium_vector-1733932442559-1e9f83edb452?q=80&w=2360&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="A random image"
-            className="w-250"
+            className="w-250 p-5 lg:p-0 rounded-lg"
           />
         </picture>
       </div>
