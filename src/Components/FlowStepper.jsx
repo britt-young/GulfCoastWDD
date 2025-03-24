983521a0652b2ad0fe42e880114862377661cd4c
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-24 13:36:39 -0400

    react icons add

--- src/Components/FlowStepper.jsx
+++ src/Components/FlowStepper.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 // import { BsPersonWorkspace } from "react-icons/bs";
 //import {UserPlusIcon} from 'heroicons/react/24/solid'
-
-
+import { IoPeopleCircleSharp } from "react-icons/io5";
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
+import { HiRocketLaunch } from "react-icons/hi2";
 
 const FlowStepper = () => {
   return (
     <div className="flex-col flex lg:flex-row mx-auto lg:gap-20 gap-0">
       {/* Step1 */}
       <div className="flex flex-col items-center mx-auto lg:max-w-7xl gap-4 py-10 lg:px-0 px-5">
-        <div className="relative">
-          <div className="lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg"></div>
+        <div className="relative lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg items-center justify-center flex">
+        <IoPeopleCircleSharp className="w-20 h-20"/>
           <span className="absolute h-15 w-15 rounded-full bg-alt border-4 border-white -top-6 -left-6 text-center justify-center flex items-center">
             <h6 className="text-main text-3xl">1</h6>
           </span>
@@ -22,8 +23,8 @@ const FlowStepper = () => {
       </div>
       {/* Step2 */}
       <div className="flex flex-col items-center mx-auto lg:max-w-7xl gap-4 py-10 lg:px-0 px-5">
-        <div className="relative">
-          <div className="lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg flex items-center justify-center"></div>
+        <div className="relative lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg items-center justify-center flex">
+        <IoShieldCheckmarkSharp className="w-20 h-20"/>
           <span className="absolute h-15 w-15 rounded-full bg-alt border-4 border-white -top-6 -left-6 text-center justify-center flex items-center">
           <h6 className="text-main text-3xl">2</h6>
           </span>
@@ -35,8 +36,8 @@ const FlowStepper = () => {
       </div>
       {/* Step3 */}
       <div className="flex flex-col items-center mx-auto lg:max-w-7xl gap-4 py-10 lg:px-0 px-5">
-        <div className="relative">
-          <div className="lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg"></div>
+      <div className="relative lg:h-60 h-30 w-30 lg:w-60 bg-white shadow-lg shadow-white/40 rounded-lg items-center justify-center flex">
+      <HiRocketLaunch className="w-20 h-20"/>
           <span className="absolute h-15 w-15 rounded-full bg-alt border-4 border-white -top-6 -left-6 text-center justify-center flex items-center">
           <h6 className="text-main text-3xl">3</h6>
           </span>
