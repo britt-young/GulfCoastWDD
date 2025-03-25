477ecf34ad532328a0c4ddff1ec0c9e536e8e926
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-25 13:51:24 -0400

    custom styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import {PhoneArrowDownLeftIcon, EnvelopeIcon, MapPinIcon} from "@heroicons/react
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 bg-[#79ABAC] p-20 lg:px-80 lg:gap-40 gap-20 z-0">
+    <div className="grid grid-cols-1 lg:grid-cols-2 bg-main dark:bg-main-dark p-20 lg:px-80 lg:gap-40 gap-20 z-0">
       <div className="flex flex-col lg:items-start justify-between py-5 lg:order-first order-last">
         <img src="https://via.placeholder.com/150" alt="logo" />
         <p className="my-5 mr-10">
@@ -13,11 +13,13 @@ const Footer = () => {
           veniam, quis nostrud exercitation ullamco laboris </p>
           <button>Get Started</button>
       </div>
-      <div className="flex flex-col lg:items-start justify-between py-5 lg:order-last order-first">
-        <h3 className="mb-5">Contact Us</h3>
+      <div className="flex flex-col lg:items-start py-5 lg:order-last order-first">
+        <h4 className="mb-4 text-2xl">Contact Us</h4>
+        <div>
         <p className="my-1"><PhoneArrowDownLeftIcon className="inline-block w-6 me-2" />+123 456 7890</p>
         <p className="my-1 mr-10"><EnvelopeIcon className="inline-block w-6 me-2" />gulfcoastwdd@protonmail.com</p>
         <p className="my-1"><MapPinIcon className="inline-block w-6 me-2" />United States</p>
+        </div>
       </div>
     </div>
   );
