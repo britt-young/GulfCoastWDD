f1e853436e344190b53fd329c4e03a1bfe239cc2
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-09-03 13:03:20 -0400

    header styling

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-import mockup from "../assets/mockup.svg"
+// import mockup from "../assets/mockup.svg"
 
 
 const HomeHeader = () => {
 
   return (
-    <div className="pt-10 pb-10 lg:px-60 flex flex-col lg:flex-row items-center justify-center gap-10 lg:h-[550px]">
+    <div className="py-10 lg:px-60 flex flex-col lg:flex-row items-center justify-center gap-10 lg:h-screen">
       {/* change below div to items-start if second div is un-commented */}
-      <div className="h-fit lg:w-[500px] items-start justify-center flex flex-col px-5 lg:px-0">
+      <div className="h-fit lg:w-1/2 items-start flex flex-col px-5 lg:px-0">
         <h4>unique designs, custom-coded</h4>
         <h1>Small business web designs</h1>
-        <p className="mt-5 ">
+        <p className="mt-8">
           No drag-and-drop webpage builders or templates. We offer 100%
           hand-coded websites for your business starting at only $145/mo with
           free hosting and edits*
@@ -19,7 +19,7 @@ const HomeHeader = () => {
           <button>get started</button>
         </Link>
       </div>
-      <div className="flex items-center lg:justify-start justify-center lg:w-[600px] lg:h-[400px] bg-white/60 border-1">
+      <div className="flex items-center lg:justify-start justify-center lg:w-1/2 lg:h-[60%] bg-white/60 border-1">
         {/* <picture>
           <img
             src={mockup}
