93c6e7fd9d72c3f322503a6cddfb6f6b6c60cef6
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-10-28 13:53:25 -0400

    workflow description text

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -16,14 +16,11 @@ const Workflow = () => {
       <div className="mx-auto max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-4 py-10 lg:px-0 px-5">
         <div className="h-full">
           <h4 className="">Our Workflow</h4>
-          <h2 className="font-bold">How we build the perfect website</h2>
+          <h2 className="font-bold ">How we build the perfect website everytime</h2>
         </div>
         <div className="flex items-center justify-start">
           <p className="border-t-1 lg:border-t-0 lg:border-l-1 border-l-0 border-alt lg:pl-5 lg:pt-0 pt-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua.
+            After deciding if our services are right for your business, we will send over our contract followed by a short questionnaire to gather all necessary information about your business. During the design process, we will expand on that information and create your site's content for you. Once approved, we build and deliver your website within 4-8 weeks.
           </p>
         </div>
       </div>
