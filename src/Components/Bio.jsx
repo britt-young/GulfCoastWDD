1110f2983d91e098f00f81c00a761e6c4ee282d0
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-10-30 18:15:55 -0400

    owner headshot swapped out

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -1,4 +1,4 @@
-import headshot from "../assets/imgs/headshot.png";
+import headshot from "../assets/imgs/owner.png";
 
 
 const Bio = () => {
@@ -10,9 +10,6 @@ const Bio = () => {
           alt="owner headshot"
           className="w-full lg:h-185 h-140 object-cover outline-2 outline-offset-1 outline-white rounded-lg"
         />
-        <p className="hidden lg:block absolute lg:bottom-2 lg:left-78  bg-white text-dark px-2 py-1 rounded-lg text-sm lg:text-base">
-          <span className="font-semibold tracking-widee">Brittney Treece</span>, Owner of Gulf Coast Web Designs & Development
-        </p> 
       </div>
       <div className="flex flex-col items-start lg:mr-90 mt-10 lg:mt-0">
         <h4 className="text-alternate">Why Choose GCWDD?</h4>
