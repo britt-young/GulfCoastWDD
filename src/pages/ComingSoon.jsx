550d3c6247f90f552cb35446141453f243620bd7
Britt Young <140761880+britt-young@users.noreply.github.com>
2026-01-26 12:42:22 -0500

    coming soon page styling

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,12 +1,20 @@
 import WaveAnimation from "../Components/WaveAnimation3";
+import { Undo2 } from "lucide-react";
 
 const ComingSoon = () => {
   return (
-    <section className="relative">
-      <WaveAnimation />
-      <div className="relative h-screen flex flex-col items-center justify-center dark:bg-dark bg-white">
-        <h4 className="text-alternate dark:text-alt">please be patient</h4>
-        <h2 className="dark:text-white">New blog articles coming soon</h2>
+    <section className="relative overflow-hidden">
+      <div className="relative h-[100px] ">
+        <WaveAnimation />
+      </div>
+      <div className="lg:px-0 relative h-100 dark:bg-dark bg-white flex flex-col items-center justify-center">
+        <div className="w-fit lg:text-start text-center">
+          <h4 className="text-alternate dark:text-alt">come back later!</h4>
+          <h3 className="dark:text-white">new blog articles coming soon</h3>
+          <button className="mt-5 hover:bg-alternate hover:text-white dark:hover:bg-white hover:dark:text-black gap-2 flex flex-row items-center justify-center w-full">
+            back to homepage <Undo2 />
+          </button>
+        </div>
       </div>
     </section>
   );
