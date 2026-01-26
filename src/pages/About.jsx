27bb84db2dcd5b4e70ff80e1606ffd09768e2d1e
Britt Young <140761880+britt-young@users.noreply.github.com>
2026-01-26 12:50:14 -0500

    react-helmet component add to each page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,16 @@ import Workflow from "../Components/Workflow";
 import PortfolioDisplay from "../Components/PortfolioDisplay";
 // import Testimonials from "../Components/Testimonials";
 import Contact from "../Components/Contact";
+import SEOHelmet from "../Components/SEOHelmet";
 
 const About = () => {
   return (
     <div className="min-h-screen">
+      {/* SEO Component */}
+      <SEOHelmet
+        title="About | Gulf Coast Web Design & Development"
+        canonical="/about"
+      />
       <AboutHeader />
       <Bio />
       <Workflow />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,17 @@
 import ContactForm from "../Components/ContactForm";
 import ContactSection from "../Components/ContactSection";
 import ContactHeader from "../Components/ContactHeader";
+import SEOHelmet from "../Components/SEOHelmet";
+
 
 const Contact = () => {
   return (
     <div className="min-h-screen ">
+      {/* SEO Component */}
+      <SEOHelmet
+        title="Contact | Gulf Coast Web Design & Development"
+        canonical="/contact"
+      />
       <ContactHeader />
       <div className="flex flex-col lg:flex-row lg:gap-20 justify-center bg-white dark:bg-dark lg:min-h-[860px]">
         <ContactSection />
