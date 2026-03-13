ccaae3004328f5cdb09b5d66438f11fd34c324a8
Britt Young <140761880+britt-young@users.noreply.github.com>
2026-03-13 14:04:44 -0400

    mockup swap and header spacing

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -1,10 +1,10 @@
-// import graphic from "../assets/mockup.svg";
+import graphic from "../assets/mockup.svg";
 import { Link } from "react-router-dom";
 
 
 export default function HomeHeader() {
   return (
-    <div className="max-w-7xl mx-auto overflow-hidden  mt-10 py-10 h-full flex flex-col lg:flex-row items-center gap-20">
+    <div className="max-w-7xl mx-auto overflow-hidden mt-10 py-10 h-100 flex flex-col lg:flex-row items-center gap-20">
       {/* Content */}
       <div className="flex flex-col text-start px-5 lg:px-0 z-0">
         <h4>unique designs, custom-coded</h4>
@@ -18,7 +18,7 @@ export default function HomeHeader() {
           <button className="w-5/6 md:w-fit">get started</button>
         </Link>
       </div>
-      <div className="w-250 h-100 hidden lg:block"></div>
+      {/* <div className="w-[80%] h-100 hidden lg:block"><img src={graphic}/></div> */}
     </div>
   );
 } 
